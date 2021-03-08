d18beb2d33dd903e20d01aa1248d56030eef401f
hfd347 <hfd347@motorolasolutions.com>
2021-03-08 21:19:46 +0100

    We have a working inference

--- CatVsDog_WithSavedModel.py
+++ CatVsDog_WithSavedModel.py
@@ -2,7 +2,31 @@ from torchvision import models, transforms
 
 import torch.nn as nn
 import torch
+import os
 from PIL import Image, ImageFile
+
+my_transform = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+
+def make_prediction(model, path_to_image):
+    model.eval()
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+    image = Image.open(path_to_image).convert("RGB")
+    transformed_image = my_transform(image)
+    transformed_image = transformed_image.unsqueeze(0) #this is adding the additional dimension
+    output = model.forward(transformed_image)
+    _, y_hat = output.max(1)
+
+    if y_hat.item() == 0:
+        print("We got a cat!")
+    elif y_hat.item() == 1:
+        print("We got a dog!")
+    else:
+        print("What do we have???")
+
 
 if __name__ == "__main__":
     path_to_model = r".\SavedModels\ModelWithCheckpoints.pth"
@@ -12,26 +36,15 @@ if __name__ == "__main__":
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 2)
     model.load_state_dict(checkpoint['model_state_dict'])
-    model.eval()
 
     # Implement passing the image to the model
-    path_to_image = r"C:\Users\HFD347\develp\pettest\oxford-iiit-pet\images\Abyssinian_107.jpg"
-    ImageFile.LOAD_TRUNCATED_IMAGES = True
-    image = Image.open(path_to_image).convert("RGB")
+    path_to_image = r"C:\Users\HFD347\develp\pettest\oxford-iiit-pet\images"
+    list_of_images = os.listdir(path_to_image)
 
-    #Inference data transform operations:
-    my_transform = transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-
-    transformed_image = my_transform(image)
-    transformed_image = transformed_image.unsqueeze(0) #this is adding the additional dimension
-
-
-    # Need to check how to convert this to probabilities of specific category
-    output = model(transformed_image)
-    print("Lets wait hear")
+    for image_name in list_of_images:
+        image_full_path = os.path.join(path_to_image, image_name)
+        print("{0} is: ".format(image_name))
+        make_prediction(model, image_full_path)
 
 '''
     # For now lets leave it and see how the checkpoint looks like
