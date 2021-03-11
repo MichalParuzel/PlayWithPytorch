4e6fab2d543a1c1d57c9881e96b7fa9431a9640a
hfd347 <hfd347@motorolasolutions.com>
2021-03-11 07:34:23 +0100

    Finished custom dataset for animal-10

--- CustomDataSet.py
+++ CustomDataSet.py
@@ -50,45 +50,32 @@ class CustomDataSet(Dataset):
 
 class CustomAnimal10DataSet(Dataset):
     def __init__(self, main_dir, transform):
-        self.all_images = []
-        self.all_images_label_map = {}
         self.main_dir = main_dir
         self.transform = transform
-        self.image_label_mapping = self.read_folders()
-        self.label_mapping = self.map_labels()
+        self.all_images = []
+        self.all_images_label_map = {}
+        self.label_mapping = {}
 
-
+        self.read_folders_and_map()
 
     def __len__(self):
         return len(self.image_label_mapping)
 
     def __getitem__(self, idx):
-        image_name = self.image_label_mapping[idx]
-        img_loc = path.join(self.main_dir, image_name)
+        image_name = self.all_images[idx]
+        label_name = self.all_images_label_map[idx]
+        img_loc = path.join(self.main_dir, label_name, image_name)
         ImageFile.LOAD_TRUNCATED_IMAGES = True
         image = Image.open(img_loc).convert("RGB")
         tensor_image = self.transform(image)
 
-        label = np.array(self.label_mapping[idx]).astype(np.int64)
+        label = np.array(self.label_mapping[label_name]).astype(np.int64)
         return tensor_image, label
 
-    def read_folders(self):
-        dir_items = os.listdir(self.main_dir)
-        labeled_dataset = {}
-        for item in dir_items:
-            if os.path.isdir(path.join(self.main_dir, item)):
-                label = item
-                category_dataset = os.listdir(path.join(self.main_dir, item))
-                for cat_item in category_dataset:
-                    if not labeled_dataset or label not in labeled_dataset:
-                        labeled_dataset[label] = []
-                    labeled_dataset[label].append(cat_item)
-        return labeled_dataset
-
-
-    #Need to test this one:
     def read_folders_and_map(self):
+        # Creates list of images and corresponding labels
         dir_items = os.listdir(self.main_dir)
+        labels_set = set()
         idx = 0
         for item in dir_items:
             if os.path.isdir(path.join(self.main_dir, item)):
@@ -97,13 +84,13 @@ class CustomAnimal10DataSet(Dataset):
                 for cat_item in category_dataset:
                     self.all_images.append(cat_item)
                     self.all_images_label_map[idx] = label
-
-    def map_labels(self):
-        self.label_mapping = {}
-        key_list = [key for key in self.image_label_mapping]
-        key_list.sort()
+                    labels_set.add(label)
+                    idx += 1
+        # Creates mapping for the labels
+        labels = list(labels_set)
+        labels.sort()
         idx = 0
-        for key in key_list:
-            self.label_mapping[key] = idx
+        for label in labels:
+            self.label_mapping[label] = np.array(idx).astype(np.int64)
             idx += 1
 
