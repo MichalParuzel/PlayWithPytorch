11926d35cddfecb188ce30ab9d5c7e15bcb17232
hfd347 <hfd347@motorolasolutions.com>
2021-03-13 16:35:13 +0100

    One more change

--- playwithpytorch/Playground.py
+++ playwithpytorch/Playground.py
@@ -4,4 +4,5 @@ import torch.nn as nn
 if __name__ == "__main__":
     path = r"C:\Users\HFD347\develp\Datasets\Animals-10"
     loss_function = nn.CrossEntropyLoss()
-    TrainingRunner.data_preparation(path, loss_function, batch_size=1, shuffle=True, num_workers=4, pretrained=True, fully_connected_layer_size=10, num_epochs=1)
+    image_dataset = CustomAnimal10DataSet(pet_image_location_all, HelperFunctions.my_transform)
+    TrainingRunner.data_preparation(image_dataset, path, loss_function, batch_size=1, shuffle=True, num_workers=4, pretrained=True, fully_connected_layer_size=10, num_epochs=1)
