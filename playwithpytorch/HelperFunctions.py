707254ac6d2ab25472faee7d32c419ceefc22738
hfd347 <hfd347@motorolasolutions.com>
2021-03-17 08:01:01 +0100

    Separate train and validate methods

--- playwithpytorch/HelperFunctions.py
+++ playwithpytorch/HelperFunctions.py
@@ -1,12 +1,125 @@
 from __future__ import print_function
 from __future__ import division
 import torch
+import torch.nn as nn
 import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
 import time
 import copy
 
+
+def train_model_new(model, criterion, optimizer, scheduler, dataloader, device, dataset_sizes, num_epochs=25, print_every=100):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    total = 0
+    correct = 0
+    wrong = 0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        model.train()
+        running_loss = 0.0
+        running_corrects = 0
+
+        # Iterate over data.
+        for inputs, labels in dataloader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward
+            # track history if only in train
+            with torch.set_grad_enabled(True):
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, labels)
+
+                total += 1
+                if preds.item() != labels.item():
+                    wrong += 1
+                else:
+                    correct += 1
+
+                loss.backward()
+                optimizer.step()
+
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+            if total % print_every == 0:
+                print("total: {0}, correct: {1} wrong {2}".format(total, correct, wrong))
+                print("Current acc: {:.4f}%".format(correct / total * 100))
+                print("Current loss: {:.4f}".format(running_loss / total))
+
+        scheduler.step()
+
+        epoch_loss = running_loss / dataset_sizes[phase]
+        epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+        print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+            phase, epoch_loss, epoch_acc))
+
+        # deep copy the model
+        #if phase == 'val' and epoch_acc > best_acc:
+        if epoch_acc > best_acc:
+            best_acc = epoch_acc
+            best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+def validate_model(model, criterion, dataloader, device, dataset_sizes, print_every=100):
+    model.eval()
+    running_loss = 0.0
+    running_corrects = 0
+    total = 0
+    correct = 0
+    wrong = 0
+
+    for inputs, labels in dataloader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # forward
+        # track history if only in train
+        with torch.set_grad_enabled(False):
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
+            total += 1
+            if preds.item() != labels.item():
+                wrong += 1
+            else:
+                correct += 1
+        # statistics
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+
+        if total % print_every == 0:
+            print("total: {0}, correct: {1} wrong {2}".format(total, correct, wrong))
+            print("Current acc: {0}%".format(correct / total * 100))
+            print("Current loss: {}".format(running_loss/ total))
+
+    val_loss = running_loss / dataset_sizes[phase]
+    val_acc = running_corrects.double() / dataset_sizes[phase]
+
+    print('Loss: {:.4f} Acc: {:.4f}'.format(val_loss, val_acc))
 
 def train_model(model, criterion, optimizer, scheduler, dataloaders, device, dataset_sizes, num_epochs=25):
     since = time.time()
@@ -173,6 +286,24 @@ my_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
+
+def load_model(device, output_size, custom=True, path_to_model=""):
+    if not custom:
+        model_ft = models.resnet18(pretrained=True)
+        num_ftrs = model_ft.fc.in_features
+        freeze_all_layers(model_ft)
+        model_ft.fc = nn.Linear(num_ftrs, output_size)
+        model_ft = model_ft.to(device)
+        return model_ft
+    else:
+        model_ft = models.resnet18(pretrained=False)
+        freeze_all_layers(model_ft)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, output_size)
+        model_state = torch.load(path_to_model, map_location=device)
+        model_ft.load_state_dict(model_state)
+        model_ft = model_ft.to(device)
+        return model_ft
 
 def freeze_all_layers(model: torch.nn.Module):
     for param in model.parameters():
