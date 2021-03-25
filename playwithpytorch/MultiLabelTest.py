5f2e3c4450f057b51f59dc54e4669bb3945f0f81
hfd347 <hfd347@motorolasolutions.com>
2021-03-25 07:49:40 +0100

    Implementing multilabel training

--- playwithpytorch/MultiLabelTest.py
+++ playwithpytorch/MultiLabelTest.py
@@ -3,6 +3,9 @@ import torch
 import os
 from CustomDataSet import MoreGenericDataset
 from torchvision import datasets, models, transforms
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
 
 def explore_dataset():
     path_to_data = r"C:\Users\HFD347\.fastai\data\pascal_2007"
@@ -63,6 +66,37 @@ def get_image_list(path_to_images):
     for item in dir_items:
         yield item
 
+def get_pretrained_model(output_size, device):
+    model_ft = models.resnet18(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+
+    model_ft.fc = nn.Linear(num_ftrs, output_size)
+    model_ft = model_ft.to(device)
+    return model_ft
+
+def train_multilabel(model, optimizer, loss_function, dataloader, epoches):
+    model.train()
+
+    for i in range(epoches):
+        for input, label in dataloader:
+            input.to(device)
+            label.to(device)
+
+            model.zero_grad() # check how to call it
+
+            with torch.set_grad_enabled(True):
+                output = model(input)
+                loss = loss_function(output, label)
+                loss.backward()
+                optimizer.step()
+
+    return model
+
+def custom_loss_function(input, target):
+    input = input.sigmoid()
+    return -torch.where(target == 1, 1-input, input).log().mean()
+
+
 test_transform = transforms.Compose([
     transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
@@ -77,4 +111,15 @@ if __name__ == "__main__":
 
     one_hot = generate_one_hot_encoding(path_to_data, "labels")
     ds = generate_dataset(path_to_data, img_list, one_hot, "test", test_transform)
+    data_loader = DataLoader(ds, batch_size=1, shuffle=True, num_workers=1)
+
+    output_size = len(one_hot[0])
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = get_pretrained_model(output_size, device)
+
+    loss_func = custom_loss_function
+    optimizer_ft = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    trained_model = train_multilabel(model, optimizer_ft, loss_func, data_loader, 1)
+
+
     print(t)
